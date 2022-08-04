e3d9a67c57fb700b9669169c134fd8223d8954c2
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2022-08-03 20:46:19 -0400

    Add current supported Drupal releases (#3)
    * Add latest official Drupal release
    
    Co-authored-by: Ofer Shaal <oshaal@phase2technology.com>

--- scripts/popup.js
+++ scripts/popup.js
@@ -109,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const moduleVersionStatus = document.getElementById('module-version');
         moduleVersionStatus.innerHTML = pageResults.moduleVersion;
 
-        const drupalCoreVersionsArray = ['9.2.0', '8.9.x', '9.0.x', '9.1.x', '9.2.x', '9.3.x'];
+        const drupalCoreVersionsArray = ['9.4.5', '9.4.x', '9.3.x', '9.5.x', '10.0.x', '10.1.x'];
         const drupalInstallProfiles = ['(none)', 'standard', 'demo_umami', 'minimal'];
         const availablePatchesArray = getPatchesFromLinks(pageResults.allHrefs);
 
